f82df40638f9a6ffeeffea87302fa40a97be5a26
Adam Boros <hunaboros@gmail.com>
2026-05-16 21:22:49 +0200

    Add a back-to-top button

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline shell for DAAD Gathering schedule viewer. Bump when assets or shell change. */
 
-const CACHE_NAME = 'daad-schedule-2026-v14';
+const CACHE_NAME = 'daad-schedule-2026-v15';
 
 const PRECACHE_URLS = [
   './index.html',
