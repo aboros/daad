a7eae3ab14ee2a1699719eadd6801e17740d163c
Adam Boros <hunaboros@gmail.com>
2026-05-16 21:41:07 +0200

    Move filters off-canvas

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline shell for DAAD Gathering schedule viewer. Bump when assets or shell change. */
 
-const CACHE_NAME = 'daad-schedule-2026-v17';
+const CACHE_NAME = 'daad-schedule-2026-v18';
 
 const PRECACHE_URLS = [
   './index.html',
