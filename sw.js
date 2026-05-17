a1d6052f7b94615a01a71750da2db2f9812e0e9f
Adam Boros <hunaboros@gmail.com>
2026-05-17 18:45:54 +0200

    Create a calendar view.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline shell for DAAD Gathering schedule viewer. Bump when assets or shell change. */
 
-const CACHE_NAME = 'daad-schedule-2026-v48';
+const CACHE_NAME = 'daad-schedule-2026-v62';
 
 const PRECACHE_URLS = [
   './index.html',
