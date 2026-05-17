1845c349422ea0e80f638363fb2e5614313ba451
Adam Boros <hunaboros@gmail.com>
2026-05-17 16:49:32 +0200

    Last effort make PWA install more obvious

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline shell for DAAD Gathering schedule viewer. Bump when assets or shell change. */
 
-const CACHE_NAME = 'daad-schedule-2026-v43';
+const CACHE_NAME = 'daad-schedule-2026-v44';
 
 const PRECACHE_URLS = [
   './index.html',
