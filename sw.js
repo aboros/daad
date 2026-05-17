028aebad47717bc71d3f635491209f9e35cf1777
Adam Boros <hunaboros@gmail.com>
2026-05-17 18:57:13 +0200

    Switch to px based hours in the calendar view

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline shell for DAAD Gathering schedule viewer. Bump when assets or shell change. */
 
-const CACHE_NAME = 'daad-schedule-2026-v62';
+const CACHE_NAME = 'daad-schedule-2026-v66';
 
 const PRECACHE_URLS = [
   './index.html',
