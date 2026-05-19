f8b7cea9e88622147b792a584ed95b51bbe69c51
Adam Boros <hunaboros@gmail.com>
2026-05-19 17:28:56 +0200

    Fix soft keyboard autoshow

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Offline shell for DAAD Gathering schedule viewer. Bump when assets or shell change. */
 
-const CACHE_NAME = 'daad-schedule-2026-v77';
+const CACHE_NAME = 'daad-schedule-2026-v78';
 
 const PRECACHE_URLS = [
   './index.html',
